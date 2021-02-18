e2393c36687ea3b052502d27c769bfc30a59ed1a
Peter Witwer <pwitwer@icloud.com>
2021-02-17 18:07:44 -0700

    Update app.js
    axes ticks are set, working on other formatting

--- D3_data_journalism/js/app.js
+++ D3_data_journalism/js/app.js
@@ -1,8 +1,8 @@
 //set margins
 // set the dimensions and margins of the graph
-var margin = {top: 10, right: 10, bottom: 30, left: 60},
+var margin = {top: 40, right: 20, bottom: 40, left: 20},
     width = 600 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    height = 600 - margin.top - margin.bottom;
 
 
 //create svg object
@@ -10,9 +10,9 @@ var svg = d3.select("#scatter")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+  
+var chartGroup =  svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 
 // d3 to select data
@@ -21,29 +21,59 @@ d3.csv("../data/data.csv").then(function(data) {
     //.get(function(error, rows) {console.log(rows); })
     console.log(data);
 
-    var x = d3.scaleLinear()
-        .domain([0, 24])
-        .range([0, width]);
-    svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
+    //parse data/cast as numbers
+    data.forEach(function(data) {
+      data.poverty = +data.poverty;
+      data.healthcare = +data.healthcare;
+    });
 
-    var y = d3.scaleLinear()
-      .domain([0, 28])
+    //create scale functions
+    var xScale = d3.scaleLinear()
+        .domain([8, d3.max(data, d => d.poverty)])
+        .range([0, width]);        
+
+    var yScale = d3.scaleLinear()
+      .domain([4, d3.max(data, d => d.healthcare) + 4])
       .range([height, 0]);
-    svg.append("g")
-      .call(d3.axisLeft(y));
 
+    //create axis functions
+    var bottomAxis = d3.axisBottom(xScale);
+    var leftAxis = d3.axisLeft(yScale);
+
+    //append axes to the chart
+    chartGroup.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+
+    chartGroup.append("g")
+      .call(leftAxis);
+
+
+    //create circles
     svg.append('g')
     .selectAll("dot")
     .data(data)
     .enter()
     .append("circle")
-      .attr("cx", d => x(d.poverty))
-      .attr("cy", d => y(d.healthcare))
-      .attr("r", 12)
+      .attr("cx", d => xScale(d.poverty))
+      .attr("cy", d => yScale(d.healthcare))
+      .attr("r", 8)
       .style("fill", "blue")
       .attr("opacity", "0.5");
+
+    //create axes labels
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", margin.left - 100)
+      .attr("x", 0-(height/2))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Lacks Healthcare (%)");
+
+    chartGroup.append("text")
+      .attr("transform", `translate(${width/2}, ${height + margin.top +30})`)
+      .attr("class", "axisText")
+      .text("In Poverty (%)");
 }).catch(function(error) {
   console.log(error);
 });
